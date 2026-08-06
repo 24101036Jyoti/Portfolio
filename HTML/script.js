d357d3020c435420ef9afb82a03bf93d276fadd6
24101036Jyoti <24101036@uap-bd.edu>
2026-08-06 21:33:38 +0600

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,33 @@
+function scrollToSection(){
+
+document.getElementById("about").scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+const sections=document.querySelectorAll(".section");
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(section=>{
+
+const top=section.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+section.classList.add("show");
+
+}
+
+});
+
+});
+
+sections.forEach(section=>{
+
+section.classList.add("hidden");
+
+});
